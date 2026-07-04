07ab8002fe643c19d4d71c0172f43d9ec109ec90
helix-nine <267227783+helix-nine@users.noreply.github.com>
2026-07-03 23:33:13 -0600

    refactor: move latest version to current.ts convention

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,6 +1,6 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
-export const v_0_11_1_1 = VersionInfo.of({
+export const current = VersionInfo.of({
   version: '0.11.1:2',
   releaseNotes: {
     en_US: 'Internal updates (start-sdk 2.0.x)',
